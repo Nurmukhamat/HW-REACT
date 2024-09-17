9a17cdc227e6370a241bd244dec664b23a742227
Nurmukhamat <gyazitdinovnurmuhamat@gmail.com>
2024-09-17 23:58:08 +0300

    create hw-project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Header } from "./components/Header";
+import { Troll } from "./components/Troll";
+function App() {
+  return (
+    <div>
+<Header />
+<Troll />
+    </div>
+    
+  )
+}
+export default App
---
+++ src/components/Troll.jsx
@@ -0,0 +1,72 @@
+import "./Troll.css"
+export const Troll = () => {
+    return (
+            <div>
+                <div className="first-container">
+                    <p>Трикотажное платье в полоску</p>
+                </div> <br />
+
+                <div className="second-container">
+                    <img src="https://cdna.lystit.com/photos/2013/04/25/asos-black-midi-bodycon-dress-in-graduated-stripe-print-product-1-8166863-103080477.jpeg" alt="" />
+                    <div classname="reduce">
+                    <p classname="sea">Цвета в наличии</p>  
+                    <ul>
+                      <li>bro</li>
+                      <li>pin</li>
+                      <li>blu</li>
+                      <li>bla</li>
+                    </ul>
+        </div>
+        <div classname="product">
+        <p classname="beast">Размеры в наличии </p>
+        <p>XXS, XS, S, M, L, XL, XXL</p>
+        </div>
+                </div> 
+
+                <div className="first-container">
+                    <p>Платье</p>
+                </div> <br />
+
+                <div className="second-container">
+                    <img src="https://cdn1.ozone.ru/multimedia/1017402966.jpg" alt="" />
+                    <div classname="reduce">
+                    <p classname="call">Цвета в наличии</p>  
+                    <ul>
+                      <li>bro</li>
+                      <li>pin</li>
+                      <li>blu</li>
+                      <li>bla</li>
+                    </ul>
+        </div>
+        <div classname="product">
+        <p classname="beast">Размеры в наличии </p>
+        <p>XXS, XS, S, M, L, XL, XXL</p>
+        </div>
+                </div> 
+
+
+                <div className="first-container">
+                    <p>Вязанный топ</p>
+                </div> <br />
+
+                <div className="second-container">
+                    <img src="https://avatars.mds.yandex.net/i?id=052fcdbf217cec0020d753817859c885_l-12612451-images-thumbs&n=13" alt="" />
+                    <div classname="reduce">
+                    <p classname="solt">Цвета в наличии</p>  
+                    <ul>
+                      <li>bro</li>
+                      <li>pin</li>
+                      <li>blu</li>
+                      <li>bla</li>
+                    </ul>
+        </div>
+        <div classname="product">
+        <p classname="beast">Размеры в наличии </p>
+        <p>XXS, XS, S, M, L, XL, XXL</p>
+        </div>
+                </div>
+
+
+            </div>
+    )
+}
