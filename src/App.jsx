815cf60bb918d387e2b129ec121738aa3031d10b
Nurmukhamat <gyazitdinovnurmuhamat@gmail.com>
2024-09-27 16:00:28 +0300

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
-import { Header } from "./components/Header";
-import { Troll } from "./components/Troll";
+
 function App() {
   return (
     <div>
-<Header />
-<Troll />
+
     </div>
     
   )
